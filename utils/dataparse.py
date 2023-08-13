232a8a46f97eccc94cbb45f8221cb843a75c6eef
super mario <mario160@mail.ru>
2023-08-13 03:50:25 +0300

    Fixes of utils

--- utils/dataparse.py
+++ utils/dataparse.py
@@ -6,11 +6,11 @@ from typing import Optional, IO
 def get_file(url: str, local_name: Optional[str] = None, mode: str = "r") -> IO:
     # Creating local name if it is not given
     if not local_name:
-        m = re.match(r"*+/([^/]*)", url)
+        m = re.match(r".+/([^/]*)", url)
         if not m or not m.group(1):
-            local_name = url
+            local_name = "./" + url
         else:
-            local_name = m.group(1)
+            local_name = "./" + m.group(1)
 
     # If there is no such file => downloading
     if not os.path.exists(local_name): # type: ignore
